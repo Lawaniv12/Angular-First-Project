422db0aec569817ee2d57ae1b4fb30d6ee9bc4ea
Victor Emmanuel Lawani <37407454+Lawaniv12@users.noreply.github.com>
2022-03-25 13:14:53 +0100

    Angular First Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { pathToFileURL } from 'url';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {  
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'contactus',
+    component: ContactUsComponent
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    DashboardComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
